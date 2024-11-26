58579436a46574f093cadcac564c2d5e1be6764b
Ginko Lee <492180240@qq.com>
2024-11-27 01:55:36 +0800

    feat: :sparkles: 专精页面

---
+++ src/data/mapSpecData.ts
@@ -0,0 +1,67 @@
+// TODO 迁移至插件内处理？
+import rawData from '@/data/spec-data.json';
+import { IWowBIS, IBisItem } from '@/interface/IWow';
+
+function getSlotLabel(key: string) {
+  type Locale = {
+    [key: string]: string;
+  };
+  const locales: Locale = {
+    head: '头部',
+    neck: '颈部',
+    shoulders: '肩部',
+    cloak: '项链',
+    chest: '胸甲',
+    wrist: '手腕',
+    gloves: '手套',
+    belt: '腰带',
+    legs: '腿部',
+    boots: '脚部',
+    ring: '戒指',
+    trinket: '饰品',
+    weapon: '武器',
+    ['main hand']: '主手',
+    ['off hand']: '副手',
+  };
+
+  return locales[key.toLowerCase() as any];
+}
+
+function getSourceLabel(source: string) {
+  if (!source) {
+    return source;
+  }
+  if (['crafting'].includes(source.toLowerCase())) {
+    return '制造';
+  }
+  return source.replace(/[a-zA-Z\s|\/]/g, '');
+}
+
+function mapBisItems(items: IBisItem[]) {
+  return items.reduce((pre: IBisItem[], cur: IBisItem) => {
+    if (cur.item && cur.slot !== 'Slot') {
+      pre.push({
+        ...cur,
+        slot: getSlotLabel(cur.slot),
+        source: getSourceLabel(cur.source),
+      });
+    }
+    return pre;
+  }, []);
+}
+
+export function mapSpecData() {
+  const data = JSON.parse(JSON.stringify(rawData)) as IWowBIS;
+  Object.entries(data).forEach((item, index) => {
+    let [key, value] = item;
+    value = value.map(specItem => ({
+      ...specItem,
+      overall: mapBisItems(specItem.overall),
+      bisItemRaid: mapBisItems(specItem.bisItemRaid),
+      bisItemMythic: mapBisItems(specItem.bisItemMythic),
+    }));
+
+    data[key] = value;
+  });
+  return data;
+}
--- src/pages/bis/index.vue
+++ src/pages/bis/index.vue
@@ -1,13 +1,43 @@
-<template></template>
+<template>
+  <uni-section class="mb-10" :class="[classKey]" title="属性优先级">
+    <uni-card>
+      <text :class="[classKey]">{{ currentData?.statsPriority }}</text>
+    </uni-card>
+  </uni-section>
+  <uni-section class="mb-10" :class="[classKey]" title="BIS配装">
+    <uni-card>
+      <view class="menu">
+        <text>汇总</text>
+        <text>大秘境</text>
+        <text>团本</text>
+      </view>
+
+      <uni-table ref="table" stripe emptyText="暂无更多数据">
+        <uni-tr>
+          <uni-th width="40" align="left">部位</uni-th>
+          <uni-th width="80" align="left">装备</uni-th>
+          <uni-th width="80" align="left">来源</uni-th>
+        </uni-tr>
+        <uni-tr v-for="(item, index) in currentData?.overall" :key="index">
+          <uni-td>{{ item.slot }}</uni-td>
+          <uni-td>{{ item.item }}</uni-td>
+          <uni-td>{{item.source}}</uni-td>
+        </uni-tr>
+      </uni-table>
+    </uni-card>
+  </uni-section>
+</template>
 
 <script lang="ts" setup>
+// TODO menu菜单样式
 import { onLoad } from '@dcloudio/uni-app';
 import { ref } from 'vue';
 
-import allData from '@/data/spec-data.json';
 import { IWowBIS, ISpceBIS } from '@/interface/IWow';
+import { mapSpecData } from '@/data/mapSpecData';
 
-const bisData = allData as IWowBIS;
+const bisData: IWowBIS = mapSpecData();
+
 const classKey = ref('');
 const specKey = ref('');
 const currentData = ref<ISpceBIS | null>();
@@ -17,8 +47,81 @@ onLoad((options: any) => {
   currentData.value = bisData[classKey.value].find(
     item => item.spec === specKey.value
   );
+
+  console.log(currentData.value);
 });
 </script>
 
-<style lang="scss"></style>
-@/interface/IWow
+<style lang="scss">
+::v-deep uni-section {
+  .uni-section-header {
+    background-color: $uni-bg-color-grey;
+    color: inherit !important;
+    .uni-section-header__content {
+      color: inherit !important;
+      .uni-section__content-title {
+        color: inherit !important;
+        text-align: center;
+        font-weight: 800;
+        font-size: 18px !important;
+        display: inline-block;
+        box-sizing: border-box;
+        &::before,
+        &::after {
+          content: '';
+          position: absolute;
+          transform: translateY(-50%);
+          width: 30%;
+          height: 2px;
+          background-color: rgb(68, 68, 68);
+        }
+        &::before {
+          left: 0;
+          top: 50%;
+        }
+        &::after {
+          right: 0;
+          top: 50%;
+        }
+      }
+    }
+  }
+  .uni-section-content {
+    background-color: $uni-bg-color-grey;
+  }
+}
+::v-deep uni-card {
+  .uni-card {
+    width: 96vw;
+    box-sizing: border-box;
+    padding: 0 !important;
+    margin: 0 auto !important;
+    border: none !important;
+    background-color: $uni-bg-color-grey-light !important;
+  }
+}
+::v-deep .uni-table {
+  background-color: rgb(40, 40, 40) !important;
+  border: 2px rgb(68, 68, 68) solid;
+  .uni-table-th,
+  .uni-table-td {
+    border-bottom: 1px $uni-bg-color solid !important;
+  }
+  .uni-table-th {
+    font-weight: 800;
+    color: #ffffff;
+  }
+  .uni-table-td {
+    font-weight: 400;
+    &:first-child {
+      color: rgb(221, 221, 221);
+    }
+    &:nth-child(2) {
+      color: rgb(163, 53, 238);
+    }
+    &:nth-child(3) {
+      color: $uni-text-color-inverse;
+    }
+  }
+}
+</style>
