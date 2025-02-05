b0fb7bf331374c58b350cb4e9a29a248f139d30f
ginko lee <492180240@qq.com>
2025-02-05 20:40:14 +0800

    设置跨域

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';  // 引入 cors 中间件
 import wowBisRoutes from './routes/wow/bisRoutes.js';
  
 const app = express();
 const port = 3000;
- 
+
+app.use(cors());
 // 使用 JSON 解析中间件
 app.use(express.json());
  
