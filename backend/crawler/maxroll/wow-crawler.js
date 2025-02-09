2758e7a1297c2a8242621cb85729b58057837a61
ginko lee <492180240@qq.com>
2025-02-09 17:04:50 +0800

    属性优先级数据进行资源化

--- backend/crawler/maxroll/wow-crawler.js
+++ backend/crawler/maxroll/wow-crawler.js
@@ -7,8 +7,12 @@ async function crawler() {
     browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
+    // 直连即可
     await page.goto(
-      'https://maxroll.gg/wow/class-guides/augmentation-evoker-mythic-plus-guide'
+      'https://maxroll.gg/wow/class-guides/augmentation-evoker-mythic-plus-guide',
+      {
+        timeout: 120000,
+      }
     );
     const html = await page.content();
     const $ = cheerio.load(html);
@@ -31,6 +35,15 @@ function getStatsPriority(context) {
     'M2 6l16 10l-16 10 M14 6l16 10l-16 10': 2,
     'M8 12l16 0M8 20l16 0': 0,
   };
+  const STAT_MAP = {
+    intellect: '智力',
+    mastery: '精通',
+    'critical strike': '暴击',
+    haste: '急速',
+    versatility: '全能',
+    strength: '力量',
+    agility: '敏捷',
+  };
   const output = [];
 
   $('div[data-wow-type="priority"]').each((index, element) => {
@@ -43,12 +56,12 @@ function getStatsPriority(context) {
     $(element)
       .find($('.mxt-stat span'))
       .each((childIndex, stat) => {
-        priority.stats.push($(stat).text());
+        priority.stats.push(STAT_MAP[$(stat).text().toLowerCase()]);
       });
     $(element)
       .find($('.mxt-relation path'))
-      .each((childIndex, stat) => {
-        priority.relations.push(ICON_MAP[$(stat).attr('d')]);
+      .each((childIndex, relation) => {
+        priority.relations.push(ICON_MAP[$(relation).attr('d')]);
       });
 
     output.push(priority);
@@ -62,7 +75,7 @@ function getStatsPriority(context) {
     .each((index, element) => {
       // 会获取到冗余的element
       if (index <= output.length - 1) {
-        output[index].talentID = $(element).attr('data-wow-id');
+        output[index].talentID = Number($(element).attr('data-wow-id'));
         output[index].talentLabel = $(element).text();
       }
     });
