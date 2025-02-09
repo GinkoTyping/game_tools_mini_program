d5c35605c47fdb0ba0c359c0cc3613a45734c5af
ginko lee <492180240@qq.com>
2025-02-09 16:03:58 +0800

    新增爬取maxroll数据

--- backend/crawler/wowhead/wow-crawler.js
+++ backend/crawler/wowhead/wow-crawler.js
@@ -1,39 +1,70 @@
-import axios from 'axios';
-import { JSDOM } from 'jsdom';
+import * as cheerio from 'cheerio';
+import puppeteer from 'puppeteer';
 
-async function crawler(params) {
+async function crawler() {
   try {
-    const response = await axios.get(
-      'https://maxroll.gg/wow/class-guides/augmentation-evoker-mythic-plus-guide',
-      {
-        headers: {
-          'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
-          'Accept-Encoding': 'UTF-8'
-        },
-      }
-    ); // 替换为你的 URL
-    const { window } = new JSDOM(response.data);
-    const document = window.document;
-    console.log(response.data);
-    console.log(JSON.stringify(document));
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
 
-    const data = getStatsPriority(document);
+    await page.goto(
+      'https://maxroll.gg/wow/class-guides/augmentation-evoker-mythic-plus-guide'
+    );
+    const html = await page.content();
+    const $ = cheerio.load(html);
+
+    const data = getStatsPriority($);
     console.log(data);
   } catch (error) {
-    console.error('Error fetching or parsing HTML:', error);
+    console.error(error);
+  } finally {
+    await browser.close();
   }
 }
 crawler();
 
-function getStatsPriority(document) {
-  const statsContainers = document.querySelectorAll(
-    'div[data-wow-type="priority"]'
-  );
-  const stats = statsContainers.querySelectorAll('.mxt-stat span');
-  const relations = statsContainers.querySelectorAll('.mxt-relation path');
-  return {
-    stats: Array.from(stats).map((item) => item.innerText),
-    relations: Array.from(relations).map((item) => item.d),
+function getStatsPriority(context) {
+  const $ = context;
+  // 0:= | 1: > | 2: >>
+  const ICON_MAP = {
+    'M8 6l16 10l-16 10': 1,
+    'M2 6l16 10l-16 10 M14 6l16 10l-16 10': 2,
+    'M8 12l16 0M8 20l16 0': 0,
   };
+  const output = [];
+
+  $('div[data-wow-type="priority"]').each((index, element) => {
+    const priority = {
+      talentLabel: '',
+      talentID: '',
+      stats: [],
+      relations: [],
+    };
+    $(element)
+      .find($('.mxt-stat span'))
+      .each((childIndex, stat) => {
+        priority.stats.push($(stat).text());
+      });
+    $(element)
+      .find($('.mxt-relation path'))
+      .each((childIndex, stat) => {
+        priority.relations.push(ICON_MAP[$(stat).attr('d')]);
+      });
+
+    output.push(priority);
+  });
+
+  $('div[data-wow-type="priority"]')
+    .first()
+    .parentsUntil('#main-article')
+    .last()
+    .find('.wow-trait')
+    .each((index, element) => {
+      // 会获取到冗余的element
+      if (index <= output.length - 1) {
+        output[index].talentID = $(element).attr('data-wow-id');
+        output[index].talentLabel = $(element).text();
+      }
+    });
+
+  return output;
 }
