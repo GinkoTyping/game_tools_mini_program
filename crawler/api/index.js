7dc7721bed6edd1dabac489e9a6aa4414edcc26e
ginko lee <492180240@qq.com>
2025-02-10 00:22:20 +0800

    爬取相关代码和后端代码独立

--- crawler/api/index.js
+++ crawler/api/index.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-configDotenv({ path: path.resolve(__dirname, '../../.env') });
+configDotenv({ path: path.resolve(__dirname, '../.env') });
 const BASE_URL = process.env.BACK_END_URL;
 
 export async function translate(input) {
