d7d17585f54e0f25e204d0a57e59d73bbb8516da
ginko lee <492180240@qq.com>
2025-02-10 14:56:39 +0800

    新增获取副本攻略；

--- crawler/maxroll/wow-crawler.js
+++ crawler/maxroll/wow-crawler.js
@@ -1,27 +1,47 @@
 import * as cheerio from 'cheerio';
 import puppeteer from 'puppeteer';
+
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import '../util/set-env.js';
 import { translate } from '../api/index.js';
 
 async function crawler() {
   let browser;
   try {
-    browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
+    let html;
 
-    // 直连即可
-    await page.goto(
-      'https://maxroll.gg/wow/class-guides/augmentation-evoker-mythic-plus-guide',
-      {
-        timeout: 120000,
-      }
-    );
-    const html = await page.content();
+    // 开发调测时，读取本地的静态文件
+    if (process.env.IS_DEV) {
+      const __filename = fileURLToPath(import.meta.url);
+      const __dirname = path.dirname(__filename);
+      html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8');
+    } else {
+      browser = await puppeteer.launch({ headless: true });
+      const page = await browser.newPage();
+
+      //  勿使用代理
+      await page.goto(
+        'https://maxroll.gg/wow/class-guides/augmentation-evoker-mythic-plus-guide',
+        {
+          timeout: 120000,
+        }
+      );
+
+      html = await page.content();
+    }
+
     const $ = cheerio.load(html);
 
-    const priority = await getStatsPriority($);
-    const ratings = getSpecRating($);
-    console.log('priority', priority);
-    console.log('ratings', ratings);
+    // const priority = await getStatsPriority($);
+    // const ratings = getSpecRating($);
+    // console.log('priority', priority);
+    // console.log('ratings', ratings);
+
+    const dungeonTips = await getDungeonTips($);
+    console.log(dungeonTips);
   } catch (error) {
     console.error(error);
   } finally {
@@ -105,6 +125,7 @@ async function getStatsPriority(context) {
       }
     });
 
+  // 翻译属性优先级的讲解文本
   async function translateDesc(statDataItem, index) {
     if (statDataItem.desc.length) {
       const data = await translate(statDataItem.desc.join('||'));
@@ -173,6 +194,84 @@ function getSpecRating(context) {
     });
 
   return ratings;
+}
+
+async function getDungeonTips(context) {
+  const $ = context;
+  const data = [];
+  $('#boss-tips-header')
+    .parent()
+    .parent()
+    .children()
+    .each((index, tipsContainer) => {
+      $(tipsContainer)
+        .children()
+        .each((index, element) => {
+          if ($(element).is('h3')) {
+            data.push({ title: $(element).text(), children: [] });
+          } else if ($(element).is('h4')) {
+            data
+              .at(-1)
+              .children.push({ title: $(element).text(), children: [] });
+          } else if ($(element).is('ul')) {
+            if (data.at(-1).children.at(-1)?.children) {
+              data.at(-1).children.at(-1).children = mapDescWithIcon(
+                $,
+                element,
+                data.at(-1).children.at(-1).children
+              );
+            } else {
+              data.at(-1).children = mapDescWithIcon(
+                $,
+                element,
+                data.at(-1).children
+              );
+            }
+
+          }
+        });
+    });
+  return data;
+}
+
+function mapDescWithIcon(context, element) {
+  const $ = context;
+  const children = [];
+  $(element)
+    .children()
+    .each((index, desc) => {
+      let totalText = $(desc).text();
+      const spellTags = [];
+      const enemySpellTags = [];
+      let liChildren = [];
+      $(desc)
+        .children('span')
+        .each((index, element) => {
+          if ($(element).find('.wow-trait').length) {
+            totalText.replace($(element).text(), `*${$(element).text()}*`);
+            spellTags.push({
+              id: $(element).find('.wow-trait').attr('data-wow-id'),
+              title: $(element).text(),
+            });
+          } else if ($(element).find('.wow-spell').length) {
+            totalText.replace($(element).text(), `@${$(element).text()}@`);
+            enemySpellTags.push({
+              id: $(element).find('.wow-spell').attr('data-wow-id'),
+              title: $(element).text(),
+            });
+          }
+        });
+      if ($(desc).find('ul').length) {
+        liChildren = mapDescWithIcon($, $(desc).find('ul'));
+      }
+      children.push({
+        totalText,
+        spellTags,
+        enemySpellTags,
+        children: liChildren,
+      });
+    });
+  return children;
 }
 
 crawler();
