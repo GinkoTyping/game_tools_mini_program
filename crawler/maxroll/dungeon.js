32a3543daea0d6f767f5e2936acb1a058d4440de
ginko lee <492180240@qq.com>
2025-03-06 04:57:20 +0800

    收到修正错误的技能翻译

--- crawler/maxroll/dungeon.js
+++ crawler/maxroll/dungeon.js
@@ -734,13 +734,13 @@ const crawlerLimiter = new Bottleneck({
 });
 async function startCrawler() {
   const mythicDungeons = [
-    // 'theater-of-pain-guide',
-    // 'the-rookery-guide',
-    // 'the-motherlode-guide',
-    // 'priory-of-the-sacred-flame-guide',
-    // 'operation-mechagon-workshop-guide',
-    // 'operation-floodgate-guide',
-    // 'darkflame-cleft-guide',
+    'theater-of-pain-guide',
+    'the-rookery-guide',
+    'the-motherlode-guide',
+    'priory-of-the-sacred-flame-guide',
+    'operation-mechagon-workshop-guide',
+    'operation-floodgate-guide',
+    'darkflame-cleft-guide',
     'cinderbrew-meadery-guide',
   ];
   const results = await Promise.allSettled(
