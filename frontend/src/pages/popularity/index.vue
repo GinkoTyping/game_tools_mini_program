b9cf22faa0a9b6a82773465972d87b94c274fe70
ginko lee <492180240@qq.com>
2025-03-06 23:13:11 +0800

    引入Lime-charts demo

--- frontend/src/pages/popularity/index.vue
+++ frontend/src/pages/popularity/index.vue
@@ -1,35 +1,141 @@
 <template>
-  <view class="charts-box">
-    <qiun-data-charts type="column" :chartData="chartData" />
+  <view>
+    <view class="title">我的主页</view>
+    <view>
+      <LEchart class="echart" ref="chart" @finished="init"></LEchart>
+    </view>
   </view>
 </template>
 
-<script lang="ts" setup>
-import { querySpecPopularity } from '@/api/wow';
-import { onLoad } from '@dcloudio/uni-app';
-import { ref } from 'vue';
+<script setup>
+import LEchart from '@/components/l-echart/l-echart.vue';
+import { onMounted, reactive, ref } from 'vue';
 
-const popularityData = ref();
-const chartData = ref({
-  categories: ['2016', '2017', '2018', '2019', '2020', '2021'],
+// TODO: 可以使用官网的定制压缩版本
+const echarts = require('../../static/echarts.min.js');
+
+let chart = ref(); // 获取dom
+const state = reactive({
+  option: {},
+});
+state.option = {
+  legend: {
+    show: true,
+    data: [],
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+    },
+  },
+  grid: {
+    left: '3%',
+    right: '8%',
+    top: '15%',
+    bottom: '5%',
+    containLabel: true,
+  },
+  xAxis: {
+    type: 'category',
+    data: [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 4, 15, 16, 17, 18, 19, 20,
+      21, 22, 23, 24,
+    ],
+    axisLabel: {
+      // inside: true,
+      // color: '#fff'
+    },
+    axisTick: {
+      show: false,
+    },
+    axisLine: {
+      show: true,
+      lineStyle: {
+        color: '#83bff6',
+      },
+    },
+    z: 10,
+  },
+  yAxis: {
+    type: 'value',
+    axisLine: {
+      show: true,
+      lineStyle: {
+        color: '#83bff6',
+      },
+    },
+    axisTick: {
+      show: false,
+    },
+    // axisLabel: {
+    //   color: '#999'
+    // },
+    splitLine: {
+      show: true,
+      lineStyle: {
+        type: 'dashed',
+        color: '#83bff6',
+      },
+    },
+  },
   series: [
     {
-      name: '目标值',
-      data: [35, 36, 31, 33, 13, 34],
-    },
-    {
-      name: '完成量',
-      data: [18, 27, 21, 24, 6, 28],
+      data: [100, 110, 113, 126, 143, 158, 165, 167, 152, 102, ,],
+      type: 'bar',
+      itemStyle: {
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          { offset: 0, color: '#83bff6' },
+          { offset: 0.5, color: '#188df0' },
+          { offset: 1, color: '#188df0' },
+        ]),
+      },
+      emphasis: {
+        itemStyle: {
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            { offset: 0, color: '#2378f7' },
+            { offset: 0.7, color: '#2378f7' },
+            { offset: 1, color: '#83bff6' },
+          ]),
+        },
+      },
+      areaStyle: {
+        show: true,
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: '#188df0',
+          },
+          {
+            offset: 1,
+            color: '#fff',
+          },
+        ]),
+      },
     },
   ],
+  color: ['#83bff6'],
+};
+// 组件能被调用必须是组件的节点已经被渲染到页面上
+onMounted(() => {
+  chart.value.init(echarts, chart => {
+    chart.setOption(state.option);
+  });
 });
-onLoad(async () => {
-  popularityData.value = await querySpecPopularity();
-});
+
+// 渲染完成
+const init = () => {
+  console.log('渲染完成');
+};
 </script>
-<style lang="scss" scoped>
-.charts-box {
+
+<style scopedlang="scss" scoped>
+.echart {
   width: 100%;
   height: 300px;
+}
+
+.title {
+  text-align: center;
 }
 </style>
