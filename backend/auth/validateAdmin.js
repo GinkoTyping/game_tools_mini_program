8d4727c5192e28cee3ce1d4322ddf22cec47c23a
ginko lee <492180240@qq.com>
2025-03-11 16:37:49 +0800

    新增一个 校验token的中间件

--- backend/auth/validateAdmin.js
+++ backend/auth/validateAdmin.js
@@ -33,3 +33,15 @@ export const validateAdmin = async (req, res, next) => {
     res.status(401).json({ message: 'Access denied' });
   }
 };
+
+export function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
--- backend/routes/wow/npcAndMarkRoutes.js
+++ backend/routes/wow/npcAndMarkRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 
 import { queryUpdateMarkStatus } from '../../controller/wow/npcAndSpellMarkController.js';
+import { authenticateToken } from '../../auth/validateAdmin.js';
 
 const router = express.Router();
-router.post('/mark/update', queryUpdateMarkStatus);
+router.post('/mark/update', authenticateToken, queryUpdateMarkStatus);
 
 export default router;
