ac97ada99d2a7af600e7a29e7fcba3fa39698df3
ginko lee <492180240@qq.com>
2025-03-13 00:37:25 +0800

    完成实时获取DPS RANK

---
+++ backend/database/wow/data/dps-rank/data.js
@@ -0,0 +1,95 @@
+import * as cheerio from 'cheerio';
+import axios from 'axios';
+
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const START_TIME = new Date('2025-03-05 8:00:00').getTime();
+function getWeekCount() {
+  return Math.ceil((Date.now() - START_TIME) / 3600 / 1000 / 24 / 7);
+}
+function getTableElements(context) {
+  const $ = context;
+  return $('.overflow-x-auto')
+    .map((index, ele) => {
+      const grid = $(ele).children().first();
+      grid.children().first().remove();
+      grid.children().first().remove();
+      return grid;
+    })
+    .get();
+}
+function mapNumber() {
+
+}
+function collectTable(context, table) {
+  const $ = context;
+  const specInfos = $(table)
+    .children('a')
+    .map((index, ele) => {
+      let [classSpec, roleClass] = $(ele)
+        .attr('href')
+        .split('/')
+        .pop()
+        .split('-');
+      if (classSpec === 'beast') {
+        classSpec = 'beast-mastery';
+        roleClass = 'hunter';
+      } else if (roleClass === 'demon') {
+        roleClass = 'demon-hunter';
+      } else if (roleClass === 'death') {
+        roleClass = 'death-knight';
+      }
+      const [avgWidth, topWidth] = $(ele).find('div[style]').map((index, bar) => {
+        return $(bar).attr('style').split(':').pop().trim().replace(';', '');
+      });
+      return {
+        avgWidth,
+        topWidth,
+        classSpec,
+        roleClass,
+      };
+    })
+    .get(); 
+  return $(table)
+    .children('div')
+    .map((index, ele) => {
+      const tds = $(ele).children('div');
+      return {
+        diff: $(tds[1]).text().trim(),
+        avg: $(tds[2]).text().trim(),
+        top: $(tds[3]).text().trim(),
+        runs: $(tds[4]).text().trim(),
+        ...specInfos[index],
+      };
+    })
+    .get();
+}
+
+async function getData(week) {
+  const period = `10${week < 10 ? `0${week}` : week}`;
+  const filePath = path.resolve(__dirname, `./cache/${period}.html`);
+
+  let html;
+  if (fs.existsSync(filePath)) {
+    html = fs.readFileSync(filePath, 'utf-8');
+  } else {
+    const res = await axios.get(
+      `https://mythicstats.com/dps?dungeon=&period=${period}`
+    );
+    html = res.data;
+
+    fs.writeFileSync(filePath, html, 'utf-8');
+  }
+
+  const $ = cheerio.load(html);
+  const tables = getTableElements($);
+  const data = tables.map((table) => collectTable($, table));
+  console.log(data);
+}
+
+getData(1);
