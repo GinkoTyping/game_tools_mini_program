e38dacac0a6431b892746906b53611d63f5daa76
ginko lee <492180240@qq.com>
2025-03-22 16:45:21 +0800

    格式化

--- frontend/src/pages/divination/index.vue
+++ frontend/src/pages/divination/index.vue
@@ -1,45 +1,28 @@
 <template>
   <view class="container">
     <view class="image-bg">
-      <image
-        src="https://ginkolearn.cyou/api/wow/assets/dungeon/wow-generic-news-image-10.webp"
-        mode="widthFix"
-      />
+      <image src="https://ginkolearn.cyou/api/wow/assets/dungeon/wow-generic-news-image-10.webp" mode="widthFix" />
       <view class="shadow-mask"></view>
     </view>
     <view class="cards-wrap">
       <view class="cards">
-        <image
-          class="center animate__animated animate__fadeInDown"
-          src="https://ginkolearn.cyou/api/wow/assets/tarot/0.jpg"
-          mode="widthFix"
-        />
-        <view
-          class="center-shadow animate__animated animate__fadeInDown"
-        ></view>
-        <image
-          class="left animate__animated animate__fadeIn animate__delay-1s"
-          src="https://ginkolearn.cyou/api/wow/assets/tarot/1.jpg"
-          mode="widthFix"
-        />
-        <image
-          class="right animate__animated animate__fadeIn animate__delay-1s"
-          src="https://ginkolearn.cyou/api/wow/assets/tarot/2.jpg"
-          mode="widthFix"
-        />
+        <image class="center animate__animated animate__fadeInDown"
+          src="https://ginkolearn.cyou/api/wow/assets/tarot/0.jpg" mode="widthFix" />
+        <view class="center-shadow animate__animated animate__fadeInDown"></view>
+        <image class="left animate__animated animate__fadeIn animate__delay-1s"
+          src="https://ginkolearn.cyou/api/wow/assets/tarot/1.jpg" mode="widthFix" />
+        <image class="right animate__animated animate__fadeIn animate__delay-1s"
+          src="https://ginkolearn.cyou/api/wow/assets/tarot/2.jpg" mode="widthFix" />
       </view>
       <view class="button" @click="drawTarot">
-        <image
-          class="animate__animated animate__fadeIn animate__delay-1s"
-          src="https://ginkolearn.cyou/api/wow/assets/tarot/button.jpg"
-        />
+        <image class="animate__animated animate__fadeIn animate__delay-1s"
+          src="https://ginkolearn.cyou/api/wow/assets/tarot/button.jpg" />
         <view class="animate__animated animate__fadeInUp animate__delay-2s">
           {{ userStore.drawTarotInfo.hasDraw ? '查看抽卡结果' : '点击抽卡' }}
         </view>
       </view>
-      <view class="footer animate__animated animate__fadeIn animate__delay-2s"
-        >{{ userStore.drawTarotInfo.totalCount ?? 0 }}人已抽</view
-      >
+      <view class="footer animate__animated animate__fadeIn animate__delay-2s">{{ userStore.drawTarotInfo.totalCount ??
+        0 }}人已抽</view>
     </view>
   </view>
   <ad-custom unit-id="adunit-19cdfb0cd4073687"></ad-custom>
@@ -85,13 +68,16 @@ $secondary-corlor: #f4e0c2;
   box-sizing: border-box;
   background-color: $bg-corlor;
   position: relative;
+
   .image-bg {
     position: relative;
     mask-image: linear-gradient(180deg, rgb(255, 255, 255) 0%, transparent 90%);
+
     image {
       width: 100vw;
     }
   }
+
   .cards-wrap {
     position: absolute;
     bottom: 0;
@@ -101,20 +87,24 @@ $secondary-corlor: #f4e0c2;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
+
     .cards {
       display: flex;
       justify-content: center;
       position: relative;
       margin-top: 20rpx;
+
       image {
         width: 30vw;
         border-radius: 16rpx;
         box-shadow: 0 0 30px 6px rgb(255 189 168 / 40%);
       }
+
       .center {
         z-index: 4;
         position: relative;
       }
+
       .center-shadow {
         width: 30vw !important;
         position: absolute;
@@ -125,35 +115,43 @@ $secondary-corlor: #f4e0c2;
         border-radius: 16rpx;
         box-shadow: 0 0 30px 6px rgba(0, 0, 0, 0.8);
       }
+
       .left,
       .right {
         width: 27vw;
         z-index: 1;
         bottom: 0rpx;
       }
+
       .left {
         position: absolute;
         left: 50%;
         transform: translateX(calc(-110% + 0rpx)) rotate(-20deg) !important;
-        transform-origin: 56% 100%; /* 旋转基准点设为底部中心 */
+        transform-origin: 56% 100%;
+        /* 旋转基准点设为底部中心 */
       }
+
       .right {
         right: 50%;
         position: absolute;
         transform: translateX(calc(110% + 0rpx)) rotate(20deg);
-        transform-origin: 44% 100%; /* 旋转基准点设为底部中心 */
+        transform-origin: 44% 100%;
+        /* 旋转基准点设为底部中心 */
       }
     }
+
     .button {
       display: flex;
       flex-direction: column;
       align-items: center;
+
       image {
         border-radius: 30rpx;
         width: 120rpx;
         height: 120rpx;
         box-shadow: 0 0 6px 2px rgba(255, 189, 168, 0.4);
       }
+
       view {
         margin-top: 20rpx;
         font-size: 32rpx;
--- frontend/src/pages/divination/result.vue
+++ frontend/src/pages/divination/result.vue
@@ -1,27 +1,17 @@
 <template>
   <view class="container">
     <view class="card-wrap" v-if="tarot.id >= 0">
-      <image
-        class="animate__animated animate__fadeInDown"
-        :src="`https://ginkolearn.cyou/api/wow/assets/tarot/${tarot.id}.jpg`"
-        mode="heightFix"
-        :class="[tarot.isPositive ? 'image--positive' : 'image--negative']"
-      />
+      <image class="animate__animated animate__fadeInDown"
+        :src="`https://ginkolearn.cyou/api/wow/assets/tarot/${tarot.id}.jpg`" mode="heightFix"
+        :class="[tarot.isPositive ? 'image--positive' : 'image--negative']" />
       <view class="main-content animate__animated animate__zoomIn">
         <view class="title">
           <text class="sub-title">您抽中了</text>
           {{ tarot.name
-          }}<text
-            class="sub-title"
-            :class="[tarot.isPositive ? 'text--positive' : 'text--negative']"
-          >
-            ({{ tarot.isPositive ? '正位' : '逆位' }})</text
-          >
+          }}<text class="sub-title" :class="[tarot.isPositive ? 'text--positive' : 'text--negative']">
+            ({{ tarot.isPositive ? '正位' : '逆位' }})</text>
         </view>
-        <view
-          class="summary"
-          :class="[tarot.isPositive ? 'text--positive' : 'text--negative']"
-        >
+        <view class="summary" :class="[tarot.isPositive ? 'text--positive' : 'text--negative']">
           {{ tarot.summary }}
         </view>
         <view class="suggestion"> {{ tarot.suggestion }} </view>
@@ -36,25 +26,12 @@
         <view id="more">
           <view class="more-button" @click="switchExpand">
             <view class="more-label">{{ isExpand ? '收起' : '更多' }}</view>
-            <uni-icons
-              :type="isExpand ? 'up' : 'down'"
-              size="20"
-              color="#ab8d60"
-            ></uni-icons>
+            <uni-icons :type="isExpand ? 'up' : 'down'" size="20" color="#ab8d60"></uni-icons>
           </view>
-          <view
-            class="fold-content animate__animated"
-            v-if="isExpand"
-            :class="[isExpand ? 'animate__fadeInUp' : '']"
-          >
-            <view
-              >今天有<text>{{ userStore.drawTarotInfo.totalCount }}人</text
-              >占卜，其中<text>{{ userStore.drawTarotInfo.count }}人</text
-              >也抽中了{{ tarot.name }}!</view
-            >
-            <view @click="clickShowAdDialog"
-              >觉得不错的话，点击<text>赏程序猿一个鸡腿🍗</text>吧</view
-            >
+          <view class="fold-content animate__animated" v-if="isExpand" :class="[isExpand ? 'animate__fadeInUp' : '']">
+            <view>今天有<text>{{ userStore.drawTarotInfo.totalCount }}人</text>占卜，其中<text>{{ userStore.drawTarotInfo.count
+            }}人</text>也抽中了{{ tarot.name }}!</view>
+            <view @click="clickShowAdDialog">觉得不错的话，点击<text>赏程序猿一个鸡腿🍗</text>吧</view>
           </view>
         </view>
       </view>
@@ -125,13 +102,16 @@ $secondary-corlor: #f4e0c2;
 $positive-color: #9bc5a0;
 $reverse-color: #fff;
 $content-font: 32rpx;
+
 .header {
   text-align: center;
+
   .info {
     font-size: 40rpx;
     color: #bbb;
   }
 }
+
 .container {
   min-height: calc(100vh - 130px);
   padding: 1rem 0;
@@ -142,47 +122,58 @@ $content-font: 32rpx;
   justify-content: center;
   text-align: center;
   background-color: $bg-corlor;
+
   .title {
     font-weight: bold;
     color: $primary-corlor;
     font-size: 48rpx;
     margin: 20rpx 0 10rpx 0;
+
     .sub-title {
       font-size: 40rpx;
       color: $secondary-corlor;
     }
   }
+
   image {
     height: 46vh;
     border-radius: 2vh;
   }
+
   .image--positive {
     box-shadow: 0 0 14px 6px rgb(135 253 72 / 32%);
   }
+
   .image--negative {
     box-shadow: 0 0 14px 6px rgb(253 72 72 / 32%);
     scale: -100% -100%;
   }
+
   .summary {
     font-weight: bold;
     font-size: 36rpx;
     color: $secondary-corlor;
   }
+
   .suggestion {
     color: $secondary-corlor;
     padding: 10rpx 20rpx;
     font-size: 32rpx;
   }
+
   .text--positive {
     color: $positive-color !important;
   }
+
   .text--negative {
     color: $color-s-tier !important;
   }
+
   .buttons {
     display: flex;
     justify-content: center;
     margin: 16rpx 0 16rpx 0;
+
     button {
       width: 220rpx;
       margin: 0 16rpx;
@@ -190,25 +181,31 @@ $content-font: 32rpx;
       font-weight: bold;
       border-radius: 10rpx;
     }
+
     #other-btn {
       background-color: $bg-light-corlor;
       color: $primary-corlor;
       border: 1px solid $primary-corlor;
     }
+
     button[open-type] {
       color: $reverse-color;
       background-color: $primary-corlor;
     }
   }
+
   #more {
     font-size: 26rpx;
+
     .more-button {
       display: flex;
       justify-content: center;
+
       .more-label {
         color: $primary-corlor;
       }
     }
+
     .fold-content {
       color: $secondary-corlor;
       text {
