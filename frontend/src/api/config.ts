4d1e7183ca2026d95363866f2a59095c9eb2b455
ginko lee <492180240@qq.com>
2025-04-01 04:13:37 +0800

    新增一个 生产环境dug的代码

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -1,7 +1,7 @@
 import { useAuth } from '@/hooks/auth';
 
-// export const BASE_URL = 'http://localhost:3000/api';
-export const BASE_URL = 'https://ginkolearn.cyou/api';
+export const BASE_URL = 'http://localhost:3000/api';
+// export const BASE_URL = 'https://ginkolearn.cyou/api';
 
 let auth;
 export const proxyRequest = async (options: any) => {
@@ -31,9 +31,21 @@ export const proxyRequest = async (options: any) => {
 
   //如果token过期 自动刷新
   if (data.statusCode === 401) {
-    auth = auth ?? useAuth();
-    await auth.silenceLogin(true);
-    return sendRequest();
+    try {
+      auth = auth ?? useAuth();
+      if (uni.getStorageSync('userId') === 1) {
+        uni.showToast({ title: 'token过期 重新请求' });
+      }
+      await auth.silenceLogin(true);
+      if (uni.getStorageSync('userId') === 1) {
+        uni.showToast({ title: 'token请求 重发请求' });
+      }
+      return sendRequest();
+    } catch (error: any) {
+      if (uni.getStorageSync('userId') === 1) {
+        uni.showToast({ title: error?.message });
+      }
+    }
   }
   return data;
 };
