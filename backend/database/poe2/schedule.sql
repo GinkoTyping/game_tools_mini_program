338ff6d2374458f644fbc259aeea37051daccae6
ginko lee <492180240@qq.com>
2025-04-04 23:36:07 +0800

    新增 ladders.updated_at

--- backend/database/poe2/schedule.sql
+++ backend/database/poe2/schedule.sql
@@ -1,6 +1,5 @@
 CREATE TABLE
   IF NOT EXISTS poe_dynamic_ladders (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
     type TEXT NOT NULL CHECK (type IN ('standard', 'hc', 'ssf', 'hc_ssf')),
     rank INTEGER NOT NULL,
     account_name TEXT NOT NULL,
@@ -8,7 +7,7 @@ CREATE TABLE
     class_name TEXT NOT NULL,
     class_name_en TEXT NOT NULL,
     level INTEGER NOT NULL,
-    experience INTEGER NOT NULL
+    experience INTEGER NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (type, rank)
   );
-
-CREATE INDEX IF NOT EXISTS idx_type_rank ON poe_dynamic_ladders (type, rank ASC);
