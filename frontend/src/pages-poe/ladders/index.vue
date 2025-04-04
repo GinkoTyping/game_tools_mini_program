5ef0f25dc7638fea0087da018f2cf04684bc8f89
ginko lee <492180240@qq.com>
2025-04-04 18:18:12 +0800

    优化UI; 广告

--- frontend/src/pages-poe/ladders/index.vue
+++ frontend/src/pages-poe/ladders/index.vue
@@ -1,44 +1,62 @@
 <template>
-  <uni-section
-    v-for="ladder in ladders?.data"
-    :key="ladder.label"
-    id="server"
-    class="priest"
-    :title="ladder.label"
-    :subTitle="ladder.desc"
-    type="line"
-    titleFontSize="16px"
-  >
-    <template v-slot:right>查看更多</template>
-    <view class="table-container">
-      <uni-table ref="table" stripe emptyText="暂无更多数据">
-        <uni-tr>
-          <uni-th
-            align="center"
-            v-for="(column, index) in displayColumns"
-            :key="column"
-            width="30"
-            >{{ column }}</uni-th
+  <view class="ladders-container">
+    <uni-section
+      v-for="ladder in ladders?.data"
+      :key="ladder.label"
+      id="server"
+      class="priest"
+      :title="ladder.label"
+      :subTitle="ladder.desc"
+      type="line"
+      titleFontSize="16px"
+    >
+      <template v-slot:right>查看更多</template>
+      <view class="table-container">
+        <uni-table ref="table" emptyText="暂无更多数据">
+          <uni-tr>
+            <uni-th
+              align="center"
+              v-for="(column, index) in displayColumns"
+              :key="column"
+              width="30"
+              >{{ column }}</uni-th
+            >
+          </uni-tr>
+          <uni-tr
+            v-for="(item, index) in ladder.data.slice(0, 10)"
+            :key="index"
           >
-        </uni-tr>
-        <uni-tr v-for="(item, index) in ladder.data.slice(0, 10)" :key="index">
-          <uni-td
-            v-for="(value, valueIdx) in displayValues(item)"
-            align="center"
-            :key="valueIdx"
-          >
-            <view :class="[`td-${valueIdx}`, 'ellipsis']">{{ value }}</view>
-          </uni-td>
-        </uni-tr>
-      </uni-table>
-    </view>
-  </uni-section>
+            <uni-td
+              v-for="(value, valueIdx) in displayValues(item)"
+              align="center"
+              :key="valueIdx"
+            >
+              <view
+                :class="[`td-${valueIdx}`, 'ellipsis']"
+                :style="{ color: accountColor(index) }"
+                >{{ value }}</view
+              >
+            </uni-td>
+          </uni-tr>
+        </uni-table>
+      </view>
+    </uni-section>
+  </view>
+
+  <view class="ad-container">
+    <ad-custom unit-id="adunit-d8d033f3e30a86cf"></ad-custom>
+  </view>
+
+  <ShareIcon />
+  
 </template>
 
 <script lang="ts" setup>
-import { getLadders } from '@/api/poe';
 import { onLoad } from '@dcloudio/uni-app';
 import { computed, ref } from 'vue';
+
+import { getLadders } from '@/api/poe';
+import ShareIcon from '@/components/ShareIcon.vue';
 
 const ladders = ref();
 onLoad(async () => {
@@ -55,9 +73,35 @@ const displayValues = computed(() => {
     return row.filter((value, idx) => ladders.value?.showIndex[idx]);
   };
 });
+const accountColor = computed(() => {
+  return index => {
+    if (index === 0) {
+      return '#d32121';
+    } else if (index <= 3) {
+      return '#e37e00';
+    } else if (index <= 5) {
+      return '#f3d037';
+    } else if (index <= 10) {
+      return '#c4ff6b';
+    } else if (index <= 20) {
+      return '#8092f1';
+    } else {
+      return 'rgb(221, 221, 221)';
+    }
+  };
+});
 </script>
 
 <style lang="scss" scoped>
+.ad-container {
+  position: fixed;
+  bottom: 42rpx;
+  left: 40rpx;
+  z-index: 2;
+}
+.ladders-container {
+  padding-bottom: 200rpx;
+}
 .table-container {
   padding: 0 20rpx;
 }
@@ -96,7 +140,6 @@ $light-border: rgb(68, 68, 68);
     }
     &:nth-child(2) {
       view {
-        color: rgb(163, 53, 238) !important;
         width: calc(100vw - 200px - 24px) !important;
       }
     }
