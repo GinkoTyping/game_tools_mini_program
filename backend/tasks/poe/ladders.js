87d682ef1cf870759da508761863fd90e150789c
ginko lee <492180240@qq.com>
2025-04-12 05:03:17 +0800

    新建定时任务

--- backend/tasks/poe/ladders.js
+++ backend/tasks/poe/ladders.js
@@ -1,8 +1,14 @@
 import schedule from 'node-schedule';
 import axios from 'axios';
-import { configDotenv } from 'dotenv';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import { configDotenv } from 'dotenv';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 configDotenv({ path: path.resolve(__dirname, '../../.env') });
+
 const complexRule = {
   hour: [0, 6, 10, 17, 22],
   tz: 'Asia/Shanghai',
