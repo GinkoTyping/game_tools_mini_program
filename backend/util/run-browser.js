3e1283ea823e15288255f0fbee2e96ccada708b8
ginko lee <492180240@qq.com>
2025-04-19 23:23:08 +0800

    连续请求可能触发反爬取

--- backend/util/run-browser.js
+++ backend/util/run-browser.js
@@ -33,7 +33,7 @@ export async function useCheerioContext(
         keepAlive: false,
       });
 
-      const res = await axiosRetry.get(url, {
+      const res = await axiosInstance.get(url, {
         headers: {
           'User-Agent':
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
