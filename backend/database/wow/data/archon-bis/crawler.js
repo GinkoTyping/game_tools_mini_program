1a5b5e4f5876274ffff6be21970dfbe6cb8dac68
ginko lee <492180240@qq.com>
2025-05-10 13:23:03 +0800

    格式

--- backend/database/wow/data/archon-bis/crawler.js
+++ backend/database/wow/data/archon-bis/crawler.js
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 function getStaticFilePath(classSpec, roleClass) {
   return path.resolve(__dirname, `./cache/${classSpec}-${roleClass}.html`);
 }
+
 function getUrl(classSpec, roleClass) {
   return `https://www.archon.gg/wow/builds/${classSpec}/${roleClass}/mythic-plus/gear-and-tier-set/high-keys/all-dungeons/this-week`;
 }
@@ -17,11 +18,10 @@ function getUrl(classSpec, roleClass) {
 function getOverviewUrl(classSpec, roleClass) {
   return `https://www.archon.gg/wow/builds/${classSpec}/${roleClass}/mythic-plus/overview/high-keys/all-dungeons/this-week#stats`;
 }
+
 function getOverviewStaticFilePath(classSpec, roleClass) {
-  return path.resolve(
-    __dirname,
-    `./cache/${classSpec}-${roleClass}-overview.html`
-  );
+  return path.resolve(__dirname,
+                      `./cache/${classSpec}-${roleClass}-overview.html`);
 }
 
 function getIdByUrl(url) {
@@ -47,21 +47,16 @@ function mapStat(val) {
 async function getStatsOverview(classSpec, roleClass, useCache) {
   const $ = await useCheerioContext(
     getOverviewStaticFilePath(classSpec, roleClass),
-    getOverviewUrl(classSpec, roleClass),
-    useCache,
-    20000
-  );
+    getOverviewUrl(classSpec, roleClass), useCache, 20000);
   const priority = $(
-    '#stats .builds-stat-priority-section>.builds-stat-priority-section__container .builds-stat-priority-section__container__inner'
-  )
+    '#stats .builds-stat-priority-section>.builds-stat-priority-section__container .builds-stat-priority-section__container__inner')
     .map((idx, ele) => {
       const key = $(ele)
         .find('.builds-stat-priority-section__container__stat-box')
         .text();
       const value = $(ele)
         .find(
-          '.builds-stat-priority-section__container__stat-box__value-wrapper'
-        )
+          '.builds-stat-priority-section__container__stat-box__value-wrapper')
         .text();
       return {
         key,
@@ -76,11 +71,8 @@ async function getStatsOverview(classSpec, roleClass, useCache) {
   priority.forEach((item, index) => {
     if (index > 0) {
       relations.push(
-        Math.abs(priority[index].value - priority[index - 1].value) >
-          Math.max(priority[index].value, priority[index - 1].value) / 10
-          ? 1
-          : 10
-      );
+        Math.abs(priority[index].value - priority[index - 1].value) > Math.max(
+          priority[index].value, priority[index - 1].value) / 10 ? 1 : 10);
     }
   });
   return {
@@ -91,15 +83,16 @@ async function getStatsOverview(classSpec, roleClass, useCache) {
 
 async function getBisOverview(classSpec, roleClass, useCache) {
   const $ = await getCheerioByPuppeteer({
-    staticFilePath: getStaticFilePath(classSpec, roleClass),
-    urlPath: getUrl(classSpec, roleClass),
-    useCache,
-    waitForSelector:
-      '#gear-tables .builds-gear-tables-section__group a a[data-disable-wowhead-tooltip=true]',
-  });
+                                          staticFilePath: getStaticFilePath(
+                                            classSpec, roleClass),
+                                          urlPath: getUrl(classSpec, roleClass),
+                                          useCache,
+                                          waitForSelector: '#gear-tables .builds-gear-tables-section__group a a[data-disable-wowhead-tooltip=true]',
+                                        });
 
   const overview = [];
   const popularTrinkets = [];
+
   function buildItemByEle(ele) {
     return {
       id: getIdByUrl($(ele).find('a').first().attr('href')),
@@ -107,6 +100,7 @@ async function getBisOverview(classSpec, roleClass, useCache) {
       popularity: $(ele).find('td').last().find('span').first().text().trim(),
     };
   }
+
   $('#gear-tables .builds-gear-tables-section__group')
     .children()
     .each((idx, slotWrap) => {
@@ -126,7 +120,13 @@ async function getBisOverview(classSpec, roleClass, useCache) {
             isPush = true;
           } else {
             if ($(trEle).find('a[data-disable-wowhead-tooltip]')?.length) {
-              isPush = true;
+              if (slotLabel === 'Off-Hand') {
+                const id = buildItemByEle(trEle).id;
+                const hasSameMainHand = overview.find(item => item.id === id);
+                isPush = !hasSameMainHand;
+              } else {
+                isPush = true;
+              }
             }
           }
 
@@ -139,12 +139,9 @@ async function getBisOverview(classSpec, roleClass, useCache) {
             }
 
             collectCount++;
-            return (
-              // 戒指部位 收录了2个以上的戒指，只收集前2
-              (slotLabel === 'Rings' && collectCount < RING_COLLECT_MAX) ||
-              // 饰品部位 需要展示不同流行度的饰品，统计5个
-              (slotLabel === 'Trinket' && collectCount < TRINKET_COLLECT_MAX)
-            );
+            return (// 戒指部位 收录了2个以上的戒指，只收集前2
+              (slotLabel === 'Rings' && collectCount < RING_COLLECT_MAX) || // 饰品部位 需要展示不同流行度的饰品，统计5个
+              (slotLabel === 'Trinket' && collectCount < TRINKET_COLLECT_MAX));
           }
 
           return true;
@@ -157,8 +154,7 @@ async function getBisOverview(classSpec, roleClass, useCache) {
     });
 
   const popularityItems = $(
-    '#gear-overview .builds-best-in-slot-gear-section__gear'
-  )
+    '#gear-overview .builds-best-in-slot-gear-section__gear')
     .children()
     .map((idx, ele) => {
       const item = $(ele).find('.gear-icon__item-name a').last();
@@ -201,11 +197,11 @@ async function getBisOverview(classSpec, roleClass, useCache) {
 
 export async function collectBisOverview(classSpec, roleClass, useCache) {
   const stats = await getStatsOverview(classSpec, roleClass, useCache);
-  const { overview, popularityItems, popularTrinkets } = await getBisOverview(
-    classSpec,
-    roleClass,
-    useCache
-  );
+  const {
+          overview,
+          popularityItems,
+          popularTrinkets
+        } = await getBisOverview(classSpec, roleClass, useCache);
 
   return {
     classSpec,
