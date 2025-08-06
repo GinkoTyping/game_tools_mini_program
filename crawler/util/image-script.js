a879f31ea9d027aa901c00d8982f36fd4f6ae656
ginko lee <492180240@qq.com>
2025-08-07 01:25:34 +0800

    feat: 更新新副本的图片

--- crawler/util/image-script.js
+++ crawler/util/image-script.js
@@ -4,9 +4,9 @@ import fs from 'fs';
 import path from 'path';
 
 // 配置参数
-const inputDir = '../../backend/assets/wow/blizz-media-image';    // 输入目录
-const outputDir = '../../backend/assets/wow/blizz-media-image-thumb';  // 输出目录
-const targetWidth = 14;      // 目标宽度
+const inputDir = '../../backend/assets/wow/dungeon-o';    // 输入目录
+const outputDir = '../../backend/assets/wow/dungeon';  // 输出目录
+const targetWidth = 400;      // 目标宽度
 
 // 确保输出目录存在
 if (!fs.existsSync(outputDir)) {
@@ -15,17 +15,17 @@ if (!fs.existsSync(outputDir)) {
 
 // 遍历输入目录中的所有WebP文件
 import('glob').then((glob) => {
-  glob.sync(`${inputDir}/**/*.jpg`).forEach(async (filePath) => {
+  glob.sync(`${inputDir}/**/*.webp`).forEach(async (filePath) => {
     try {
       const outputPath = path.join(outputDir, path.basename(filePath));
-      
+
       await sharp(filePath)
         .resize(targetWidth)
         .toFile(outputPath);
-      
+
       console.log(`成功处理: ${filePath} -> ${outputPath}`);
     } catch (error) {
       console.error(`处理失败: ${filePath}`, error.message);
     }
   });
-})
+});
