4c975743047d288c1ca0746d3c6c4021f2da9a3a
ginko lee <492180240@qq.com>
2025-08-17 17:56:16 +0800

    更新 大秘境攻略的文本 缺失翻译的小怪名称

--- backend/database/wow/mapper/npcMapper.js
+++ backend/database/wow/mapper/npcMapper.js
@@ -3,53 +3,62 @@ let db;
 async function insertNpc(params) {
   const { id, name_zh, name_en, location, type } = params;
   return db.run(
-    `INSERT INTO wow_npc(id, name_zh, name_en, location, type) VALUES(?1, ?2, ?3, ?4, ?5)`,
-    [id, name_zh, name_en, location, type]
+    `INSERT INTO wow_npc(id, name_zh, name_en, location, type)
+     VALUES (?1, ?2, ?3, ?4, ?5)`,
+    [id, name_zh, name_en, location, type],
   );
 }
 
 async function getNpcById(id) {
-  return db.get(`SELECT * FROM wow_npc WHERE id=?1`, [id]);
+  return db.get(`SELECT *
+                 FROM wow_npc
+                 WHERE id = ?1`, [id]);
 }
 
 async function updateNpc(params) {
   const { id, name_zh, name_en, location, type } = params;
   return db.run(
     `UPDATE wow_npc
-    SET name_zh = CASE
-      WHEN ?1 IS NOT NULL THEN ?1
-      ELSE name_zh
-    END,
-    name_en = CASE
-      WHEN ?2 IS NOT NULL THEN ?2
-      ELSE name_en
-    END,
-    location = CASE
-      WHEN ?3 IS NOT NULL THEN ?3
-      ELSE location
-    END,
-    type = CASE
-      WHEN ?4 IS NOT NULL THEN ?4
-      ELSE type
-    END
-    WHERE id=?5
-  `,
-    [name_zh, name_en, location, type, id]
+     SET name_zh  = CASE
+                        WHEN ?1 IS NOT NULL THEN ?1
+                        ELSE name_zh
+         END,
+         name_en  = CASE
+                        WHEN ?2 IS NOT NULL THEN ?2
+                        ELSE name_en
+             END,
+         location = CASE
+                        WHEN ?3 IS NOT NULL THEN ?3
+                        ELSE location
+             END,
+         type     = CASE
+                        WHEN ?4 IS NOT NULL THEN ?4
+                        ELSE type
+             END
+     WHERE id = ?5
+    `,
+    [name_zh, name_en, location, type, id],
   );
 }
 
 async function getNpcWithNoNameZh() {
-  return db.all(`SELECT * FROM wow_npc WHERE name_zh IS NULL`);
+  return db.all(`SELECT *
+                 FROM wow_npc
+                 WHERE name_zh IS NULL
+                    OR name_zh NOT GLOB '*[一-龥]*'`);
 }
 
 async function getNpcByNameEn(nameEn) {
-  return db.get(`SELECT * FROM wow_npc WHERE name_en COLLATE NOCASE =?1`, [
+  return db.get(`SELECT *
+                 FROM wow_npc
+                 WHERE name_en COLLATE NOCASE = ?1`, [
     nameEn,
   ]);
 }
 
 async function getAllNpc() {
-  return db.all(`SELECT * FROM wow_npc`);
+  return db.all(`SELECT *
+                 FROM wow_npc`);
 }
 
 export function useNpcMapper(database) {
